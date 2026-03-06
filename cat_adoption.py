71c327ba3b2eabde1366771917f7d234c6f0c365
amyotwaybjss <amy.otway@cgi.com>
2026-03-06 16:51:31 +0000

    Added emojis

--- cat_adoption.py
+++ cat_adoption.py
@@ -10,6 +10,13 @@ image_location = '../images/'
 import_file = 'cat_database.xlsx'
 output_file_location = 'output/'
 
+cat_colour = {
+    "orange" : "🧡",
+    "black" : "🖤",
+    "grey" : "🩶",
+    "white" : "🤍"
+}
+
 cat_database = pd.ExcelFile(import_file)
 cat_sheet = cat_database.parse(cat_database.sheet_names[0])
 cats_dict = cat_sheet.T.to_dict()
@@ -21,10 +28,10 @@ template = environment.get_template('cat_page.html')
 for cat in cats_list:
     filename = f'page_{cat['Name']}.html'
     output_file = output_file_location + filename
-
     content = template.render(
         cat,
         image_location=image_location,
+        colour_emoji = cat_colour[cat['Colour'].lower()],
     )
     with open(output_file, mode='w') as page:
         page.write(content)
