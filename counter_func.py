3aa5446355017afdb374723664b1c6769f09a50c
amyotwaybjss <amy.otway@cgi.com>
2026-04-22 16:20:57 +0100

    Refactoring counter_func.py

--- counter_func.py
+++ counter_func.py
@@ -1,37 +1,29 @@
-# from cat_adoption import cats_list
-
-cats_list = ['A', 'B', 'C', 'D', 'E','F','G','H','I','J','K','L']
+# cats_list = ['A', 'B', 'C', 'D', 'E','F','G','H','I','J','K','L']
+cats_list = ['A', 'B']
+# fix 2/3 case
 checked_numbers = [3, 2]
 
-count = len(cats_list)
+# count = len(cats_list)
+for i in range(1, 200):
+    count = i
 
-response = []
+    response = []
+    potential_cols = []
+    offset_flag = []
 
-if count < 1:
-    response = f'{count} is less than than 1.'
+    if count < 1:
+        response = f'{count} is less than than 1.'
+    else:
+        for number in checked_numbers:
+            if count % number == 0:
+                response.append(f'{count} is divisible by {number}.')
+                potential_cols.append(number)
+                offset_flag.append(False)
+            elif (count+1) % number == 0:
+                response.append(f'{count}+1 is divisible by {number}.')
+                potential_cols.append(number)
+                offset_flag.append(True)
 
-for number in checked_numbers:
-    if count % number == 0:
-        response.append(f'{count} is divisible by {number}.')
-    elif (count+1) % number == 0:
-        response.append(f'{count}+1 is divisible by {number}.')
+    result = [i, response, response[0], potential_cols, potential_cols[0], offset_flag, offset_flag[0]]
 
-print(response)
-print(response[0])
-
-
-# if count < 1:
-#     print(f'{count} is less than than 1.')
-# elif count % 3 == 0:
-#     print(f'{count} is divisible by three.')
-# elif count % 2 == 0:
-#     print(f'{count} is divisible by two.')
-# elif (count+1) % 3 == 0:
-#     print(f'{count}+1 is divisible by three.')
-# elif (count+1) % 2 == 0:
-#     print(f'{count}+1 is divisible by two.')
-# else:
-#     print(f'{count} is invalid.')
-
-# function takes in a LIST of items and counts them
-# it then needs to compare to another list of numbers and see where the first case of it being divisible is.
+    print(result)
